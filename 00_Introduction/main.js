5c215d18a52d9ea967f03b8f9a22535a93618c4c
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-27 20:20:20 +0500

    setup started

---
+++ 00_Introduction/main.js
@@ -0,0 +1,23 @@
+/*
+    Why react?
+        1. Single Page Applications.
+        2. Multi Page Applications.
+
+    What is React?
+        React is UI Library based on Components.
+
+    DOM (Document Object Model)
+        Basically, DOM is Document Object Model where we have parent and child elements.
+        Document is an object.
+
+    Components
+        1. Easy Structure
+        2. Re-Use
+        3. HTML, CSS, JavaScript
+
+    Setup
+        NodeJS
+        Browser
+        Code Editor
+
+*/
