87e36ce78344dd7b3b45b23114fc67d42b24f7e9
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-04 01:17:05 +0500

    What are events

--- Temp/main.js
+++ Temp/main.js
@@ -1,7 +1,8 @@
 /*
 
-    Conditional Rendering.
-    Conditional rendering is basically rendering layout on the basis of some conditions.
-    We can do that using JavaScript.
+    Events
+    Event is basically when something happens by user.
+    Like mouse click, mouse over to some element, mouse out from somewhere, etc...
+    Keyboard click, button up, button press, key down, etc...
 
 */
