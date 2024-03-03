48788430adf40aa128d5e0d179c3cdca85c2a043
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-04 02:19:44 +0500

    Event Bubbling

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,20 +1,11 @@
 import Video from "./components/Video";
-import Com from "./components/Com";
 import "./App.css";
 import data from './data/data'
 import PlayButton from "./components/PlayButton";
 
-// Also data outside the src folder is not supported.
+// Event Bubbling will happen same as HTML.
 
 function App() {
-    // As this is JSX, we can use JavaScript things as well. Like passing props using javaScript Object and then using spread operator.
-    // names should match 100%.
-    // We can use higher-order functions like map.
-    // It'll take an callback function which return JSX. Using an iterator.
-
-    // In data, there should be an ID or any unique property.
-    // Basically a key prop.
-    // It's name should be "key" key prop. We're not gonna use it or we can but not key but another id propery with same value. But still, it is important.
     return (
         <>
             <div className="App">
