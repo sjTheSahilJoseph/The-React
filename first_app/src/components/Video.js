61d9ed2de9d32fa80aee90eae6b20eb658e0f91e
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-04 00:02:25 +0500

    Ternary working

--- first_app/src/components/Video.js
+++ first_app/src/components/Video.js
@@ -6,9 +6,10 @@ function Video(props) {
     let channel = props.channel;
     let views = props.views;
     let time = props.time;
-    let channelJSX;
     let verified = props.verified;
 
+    // We can also pass null in ternary operator.
+    // Why it is working? Because ternrary returns the jsx. But if else don't.
     return (
 
         <>
@@ -19,6 +20,7 @@ function Video(props) {
                 <div className='title'>{title}</div>
 
         { verified ? <div className='channel'>{channel} verified</div> :  <div className='channel'>{channel}</div>}
+
                 <div className='views'>{views} views <span>.</span> {time}</div>
             </div>
         </>
