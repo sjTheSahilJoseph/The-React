54b2e543b672516dd7c9172f1067790f28fc0ee3
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-03 23:23:06 +0500

    Passing data as Object and then spread operator

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -2,12 +2,20 @@ import Video from "./components/Video";
 import "./App.css";
 
 function App() {
+    // As this is JSX, we can use JavaScript things as well. Like passing props using javaScript Object and then using spread operator.
+    let data = {
+        title: "title x",
+        channel: "chanell x",
+        time: "25 minutes ago",
+        views: "1M views"
+    }
     return (
         <>
             <div className="App">
                 <Video title="Title 1" channel="SJ" time="1 minute ago" views="10k" />
                 <Video title="Title 2" channel="JS" views="1k" time="10 minutes ago" />
                 <Video channel="JS" views="1k" time="10 minutes ago" />
+                <Video {...data}/>
             </div>
         </>
     );
