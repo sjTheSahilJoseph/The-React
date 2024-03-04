58e3dae82380463d466fe5c2ffb14c057517ee3e
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-05 02:39:56 +0500

    So many times state change no effect

--- first_app/src/components/Counter.js
+++ first_app/src/components/Counter.js
@@ -1,37 +1,20 @@
 import { useState } from "react";
 
 function Counter() {
-    // Keep in mind that if we print the value of number, it'll tell its previous, because of the control flow.
-    // Simple as that.
-
-    // Normal Variable.
-    // let number = 0;
-    
-    // State Variable.
     const [number, setNumber] = useState(0);
-    // Name is just convention. But it is more common to use setName, name. like this
-    // This syntax is 'destructured array'.
-    // We need to make two things in array, 1 is the state variable, second is function to update its value.
-    // Then we need to use useSate(default initial value);
-    // setNumber is called "setter function".
 
     function handleIncrement(e) {
 
         e.stopPropagation();
 
-        // Normal way to increment normal variable.
-        // number++;
-        // Now it'll increment but the UI won't render its updated value.
-        // So in order to update the UI with latest value, we need to make it a state variable using useState hook.
+        setNumber(number + 1);
+        setNumber(number + 1);
+        setNumber(number + 1);
+        setNumber(number + 1);
+        setNumber(number + 1);
+        // It is incrementing by 1. It doesn't matter how many times we change state in click, Because on first time change it re-renders the whole component with the value.
+        // And then we again update the state by pressing the button, and the things is other setNubmers are not even using.
 
-
-        // State Update.
-        // number++; -> wrong, we can't change state directly
-        setNumber(number+1);
-
-        // Now it'll work fine.
-        // Basically react will re-render the component with the updated state value.
-        // When state will update, the component will re-render.
 
         console.log("handleIncrement");
 
@@ -41,8 +24,8 @@ function Counter() {
 
         <>
 
-            <h1 style={{color: 'white'}}>{number}</h1>
-            
+            <h1 style={{ color: 'white' }}>{number}</h1>
+
             <button onClick={handleIncrement}>Increment</button>
 
         </>
