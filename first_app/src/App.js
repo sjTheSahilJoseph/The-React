45b2ce69ae72fe8f79dc30a727a32f63a38b6feb
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-03-05 03:03:29 +0500

    copy first render second

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,13 +1,39 @@
 import Video from "./components/Video";
 import "./App.css";
-import data from './data/data'
+import dataa from './data/data'
 import PlayButton from "./components/PlayButton";
 import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+    const [data, setData] = useState(dataa);
+
+    function handleAddVideo(e) {
+        e.stopPropagation();
+        
+        data.push(
+    {
+        id: 1,
+        title: "title x",
+        channel: "chanell x",
+        time: "25 minutes ago",
+        views: "1M views",
+        verified: true,
+    }
+        );
+        // We can't mutate state variable directly
+        // React want that first it makes a copy or previous state, then update by updated state and then updated state will stored as previous state for next time.
+
+
+        console.log("handleAddVideo");
+    }
+
     return (
         <>
             <div className="App" onClick={() => { console.log("app") }}>
+        <div>
+            <button onClick={handleAddVideo}>Add Video</button>
+        </div>
                 {
                     data.map((video) => {
                         return (
